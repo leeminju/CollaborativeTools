9157b5e7fa885710d9821ea862a91e2786b39d6e
leeminju <leemj4090@naver.com>
2023-12-26 17:55:24 +0900

    init commit

---
+++ src/main/java/com/example/collaborativetools/CollaborativeToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.collaborativetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaborativeToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/collaborativetools/CollaborativeToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collaborativetools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
