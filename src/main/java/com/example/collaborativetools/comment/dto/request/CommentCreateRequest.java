15bef31db3f2401ee73c88f30cb94cd322223cab
MinJung Kim <kminss3711@gmail.com>
2024-01-02 11:19:16 +0900

    Refactor: 댓글 API url 변경에 따른 기능 구현 리팩토링

--- src/main/java/com/example/collaborativetools/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/com/example/collaborativetools/comment/dto/request/CommentCreateRequest.java
@@ -4,7 +4,6 @@ import com.example.collaborativetools.card.entitiy.Card;
 import com.example.collaborativetools.comment.entitiy.Comment;
 import com.example.collaborativetools.user.entitiy.User;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +11,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class CommentCreateRequest {
-	@NotNull
-	private final Long boardId;
-	@NotNull
-	private final Long cardId;
 	@Size(max = 250)
 	private final String comment;
 
--- src/main/java/com/example/collaborativetools/comment/dto/request/CommentUpdateRequest.java
+++ src/main/java/com/example/collaborativetools/comment/dto/request/CommentUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.example.collaborativetools.comment.dto.request;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +7,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class CommentUpdateRequest {
-	@NotNull
-	private final Long boardId;
-	@NotNull
-	private final Long cardId;
 	@Size(max = 250)
 	private final String comment;
 }
